bbf57ad7661bec5717b8e3c13e41797d794492a6
torr <newmyclio2004>
2018-12-31 18:52:11 +0200

    main page was changed

--- system/application/controllers/index.php
+++ system/application/controllers/index.php
@@ -4,21 +4,33 @@
  * @copyright 2010
  */
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Index extends MY_Controller 
-    {
-    
-    public function __construct()
-	{
-        parent::MY_Controller();
-    }
-    
-	function index()
-	{
-       $this->data_arr['title'] = 'Àøîò Àðóòþíÿí';
-       $data = array(
-                  'content' => $this->load->view('index/show',$this->data_arr, true));
+class Index extends MY_Controller
+{
+    public $data_arr;
 
-	   $this->load->view('layout_index', $data);
-       parent::MY_Controller();
-	}
-	}
+    public function __construct()
+    {
+        parent::MY_Controller();
+        $this->data_arr['title'] = '';
+        $this->data_arr['uri']   = explode('/',$_SERVER['REQUEST_URI']);
+    }
+
+    function index()
+    {
+//       $this->data_arr['title'] = '???? ????????';
+//       $data = array(
+//                  'content' => $this->load->view('index/show',$this->data_arr, true));
+//
+//	   $this->load->view('layout_index', $data);
+//       parent::MY_Controller();
+        $this->data_arr['result'] = $this->about_model->getListJoin();
+        //get list of links for current page
+        $this->data_arr['about_link'] = $this->about_model->getArrWhere('about_links',array('status' => 'on'), null, null, 'id DESC');
+        $this->data_arr['about_contact'] = $this->about_model->getArrWhere('contacts',array(), null, null);
+        $data = array(
+            'menu'    => $this->load->view(MENU, '', true),
+            'content' => $this->load->view('about/show', $this->data_arr, true));
+
+        $this->load->view('layout', $data);
+    }
+}
